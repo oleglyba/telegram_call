6751183cba0e4105e350edf493de83e2f5e282ec
oleglyba <libaoleg8@gmail.com>
2025-03-11 17:55:45 +0200

    congratulation

--- src/components/Congratulations/Congratulations.js
+++ src/components/Congratulations/Congratulations.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./Congratulations.css";
 
 const Congratulations = () => {
-
+    const handleClose = () => {
+        window.location.href = process.env.REACT_APP_RETURN_BOT_LINK;
+    };
 
     return (
         <div className="form-container">
@@ -11,6 +13,9 @@ const Congratulations = () => {
                 <p>
                     You have successfully registered on our web app, now you can use all «Telegram» functions
                 </p>
+                <button className="close-btn" onClick={handleClose}>
+                    Close
+                </button>
             </div>
         </div>
     );
