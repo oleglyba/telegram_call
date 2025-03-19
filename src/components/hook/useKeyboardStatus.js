1e6f53b1fcd492e3ca769ba3cde2a47ec7ca1831
oleglyba <libaoleg8@gmail.com>
2025-03-19 13:37:47 +0200

    keyboard-open dynamic

--- src/components/hook/useKeyboardStatus.js
+++ src/components/hook/useKeyboardStatus.js
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 
 export default function useKeyboardStatus() {
-    const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+    const [keyboardHeight, setKeyboardHeight] = useState(0);
 
     useEffect(() => {
         const handleResize = () => {
             if (window.visualViewport) {
-                setIsKeyboardOpen(window.visualViewport.height < window.innerHeight * 0.75);
+                const heightDifference = window.innerHeight - window.visualViewport.height;
+                setKeyboardHeight(heightDifference > 0 ? heightDifference : 0);
             }
         };
 
         window.visualViewport?.addEventListener("resize", handleResize);
-        return () => window.visualViewport?.removeEventListener("resize", handleResize);
+        handleResize();
+
+        return () =>
+            window.visualViewport?.removeEventListener("resize", handleResize);
     }, []);
 
-    return isKeyboardOpen;
+    return keyboardHeight;
 }
